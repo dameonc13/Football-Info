58e043adbe5d709ed92e222c308857e21fd60111
Dameon Charleygit config --global user.email dameoncharley13@hotmail.comgit config --global user.name Dameon <dameoncharley13@hotmail.com>
2020-05-13 13:54:36 -0400

    Updates

---
+++ precache-manifest.1219c9c0f75bd26abd90099e27263f89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "749c4f94c0bfa674e2f436e3ea8ce66a",
+    "url": "/Football-Info/index.html"
+  },
+  {
+    "revision": "4314571cdd7ff0aa0c99",
+    "url": "/Football-Info/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "13390d85d5f09aca08f5",
+    "url": "/Football-Info/static/js/2.56f430de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Football-Info/static/js/2.56f430de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4314571cdd7ff0aa0c99",
+    "url": "/Football-Info/static/js/main.9819eb35.chunk.js"
+  },
+  {
+    "revision": "9aade772445fcd3487a2",
+    "url": "/Football-Info/static/js/runtime-main.45ac196d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Football-Info/static/media/logo.5d5d9eef.svg"
+  }
+]);
